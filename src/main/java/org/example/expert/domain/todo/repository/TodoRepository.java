4d6b846df4bb56f502931274547910a66a52a426
박병천 <genius178896@gmail.com>
2025-01-02 20:54:00 +0900

    Refactor : LV4 : N+1 문제 개선
    - fetch join을 사용하여 해결

--- src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
+++ src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
@@ -4,9 +4,11 @@ import org.example.expert.domain.todo.entity.Todo;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface TodoRepository extends JpaRepository<Todo, Long> {
 
+    @Query("select t from Todo t join fetch t.user")
     Page<Todo> findAllByOrderByModifiedAtDesc(Pageable pageable);
 
     int countById(Long todoId);
